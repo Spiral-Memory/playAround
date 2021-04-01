c9045e1eaa32bf777cc1d5c6e8bc13b2a7eaf53c
Zishan <zishanahmad20bcs78@iiitkottayam.ac.in>
2021-04-01 17:27:42 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const io = require('socket.io')(process.env.PORT);
+const users =  {}
+
+io.on('connection',socket =>{
+     console.log('New User')
+     socket.on('send-username',nameofuser =>{
+         users[socket.id] = nameofuser;
+         socket.broadcast.emit('user-connected',nameofuser)
+     })
+      socket.on('send-chat-message',datamessage => {
+           socket.broadcast.emit('chat-message',{datamessage:datamessage.message,name1:datamessage.nameofme});
+    })
+
+    // socket.on('disconnect',()=>{
+    //    socket.broadcast.emit('user-disconnected',users[socket.id])
+    //    delete users[socket.id];
+
+    // })
+})
+
